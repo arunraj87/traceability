81cb965aa76c27d2731a84fd583b3e77f4309c5c
arunraj87 <arunrajendran1987@gmail.com>
2019-10-03 19:01:44 +0530

    Add files via upload

--- timeline.js
+++ timeline.js
@@ -12,15 +12,12 @@
         var tempateToParse = defaults.rawTemplate.status;
         data.forEach(function(value, index){
             tempateToParse = defaults.rawTemplate.status;
-            tempateToParse = tempateToParse.replace('##index##', index);
-            tempateToParse = tempateToParse.replace('##index##', index);
-            tempateToParse = tempateToParse.replace('##index##', index);
-            tempateToParse = tempateToParse.replace('##index##', index);
-            tempateToParse = tempateToParse.replace('##index##', index);
+            tempateToParse = tempateToParse.replace(/##index##/g, index);
             tempateToParse = tempateToParse.replace('##status##', value.status);
             tempateToParse = tempateToParse.replace('##date##', new Date().toDateString());
             tempateToParse = tempateToParse.replace('##comment##', value.comment);
             tempateToParse = tempateToParse.replace('##completed##', value.completed ? 'indicator-completed':'');
+            tempateToParse = tempateToParse.replace(/##status-completed##/g, value.completed ? 'status-completed':'');
             if(data[index+1] && data[index+1].completed) {
                 tempateToParse = tempateToParse.replace('##connector-completed##', value.completed ? 'connector-completed' : '');
             }
@@ -32,17 +29,17 @@
         var parentContainerDOM = '<div class="container col-sm-12"></div>';
         var contentDOM = '<div class="content"></div>';
         var statusTemplate = '<div class="link">' +
-                                '<div class="status status-transition top-content" style="transition-delay:##index##s">' +
+                                '<div class="status status-transition top-content ##status-completed##" style="transition-delay:##index##s">' +
                                     '<span>##status##</span>' +
                                 '</div>' +
                                 '<div class="indicator ##completed##" style="transition-delay:##index##s"></div>' +
                                 '<div class="connector-container">' +
                                     '<div class="##connector-completed##" style="transition-delay:##index##s;"></div>' +
                                 '</div><div class="bottom-content">' +
-                                '<div class="status status-transition" style="transition-delay:##index##s">' +
+                                '<div class="status status-transition ##status-completed##" style="transition-delay:##index##s">' +
                                     '<span>##date##</span>' +
                                 '</div>' +
-                                '<div class="status status-transition" style="transition-delay:##index##s">' +
+                                '<div class="status status-transition ##status-completed##" style="transition-delay:##index##s">' +
                                     '<span>##comment##</span>' +
                                 '</div>' +
                              '</div></div>';
